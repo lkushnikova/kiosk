bf9571a2eb187f4b1589df1629925b50a96fc2ab
Helena Kushnikova <lkushnikova@gmail.com>
2017-08-02 16:58:40 +0300

    18 july 2017 11:38

--- choosing_datetime.php
+++ choosing_datetime.php
@@ -16,36 +16,37 @@ try {
         PDO::ATTR_EMULATE_PREPARES   => false,
     ];
     $conn = new PDO($dsn, $user, $pass, $opt);
-    $stmt = $conn->prepare('SELECT * FROM schedule WHERE dct_id=:id GROUP BY date');
+    $set_local = $conn->prepare('SET lc_time_names="ru_RU"');
+    $set_local->execute();
+    $stmt = $conn->prepare('SELECT schedule_id,dct_id, DATE_FORMAT(date,"%d.%m.%Y") AS date, DATE_FORMAT(date,"%W") AS weekday,time,special_id,status FROM schedule WHERE dct_id=:id GROUP BY date');
     $stmt->bindParam(':id', $id);
     $stmt->execute();
     $result = $stmt->fetchAll();
-    $doctor = $conn->prepare('SELECT * FROM doctor WHERE dct_id=:id ');
+    $doctor = $conn->prepare('SELECT * FROM doctor JOIN special ON doctor.special_id=special.special_id WHERE dct_id=:id ');
     $doctor->bindParam(':id', $id);
     $doctor->execute();
     $doctor_result = $doctor->fetchAll();
 
+
 ?>
   <div class="big_banner" style="background-color:transparent; box-shadow:none;">
   <h1 class="for_h1_info">Запись к врачу</h1>
-<div class="left_menu_about">
-<div class="for_but for_but_menu"><a href="docs.php">Список<br>ВРАЧЕЙ</a></div>
-</div>
-<div class="center_content">
+<div class="news white_news">
 	<?php
     if ( count($doctor_result) ) {
         foreach($doctor_result as $doctor_row) {
             $doc=$doctor_row["dct_lastname"].' '.$doctor_row["dct_firstname"].' '.$doctor_row["dct_midname"];
-            echo '<h4>'.$doc.'</h4>';
+            echo '<h3 class="doctor_name" style="">'.$doc.'<strong>,'.$doctor_row["special_name"].'</strong></h3>';
         }
     } else {
         echo "Ничего не найдено.";
     }
-    echo  '<p>Выберите дату</p>';
+
     if ( count($result) ) {
+        echo  '<p class="big_p">Выберите дату и время</p>';
         foreach($result as $row) {
 
-            echo '<div class=" for_but for_datetime"><a href="#.php">'.$row['date'].'</a></div>';
+            echo '<div class=" for_but for_datetime"><a href="#.php">'.$row['date'].'<br>'.$row['weekday'].'</a></div>';
 
 }
     } else {
